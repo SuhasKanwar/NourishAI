b6906f21ed80eb68a5947955e02ae80ce1d4168f
Suhas Kanwar <suhas.kanwar@gmail.com>
2026-05-02 01:42:43 +0530

    done with the basic setup

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,50 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "NourishAI",
+  description: "Autonomous AI system that plans and manages daily food, groceries, and dining decisions using LLMs and multi-agent reasoning. It learns user habits, optimizes budget and health, and executes actions via Swiggy MCP APIs, shifting from passive suggestions to proactive, real-world automation.",
+  authors: [
+    {
+      name: "Suhas Kanwar",
+      url: "https://github.com/SuhasKanwar"
+    },
+    {
+      name: "Suhas Kanwar",
+      url: "https://suhaskanwar.vercel.app"
+    }
+  ],
+  keywords: ["AI", "Autonomous Agents", "LLMs", "Multi-Agent Systems", "Food Management", "Grocery Planning", "Dining Decisions", "Health Optimization", "Budget Optimization", "Swiggy MCP APIs"]
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      data-scroll-behavior="smooth"
+    >
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen bg-(--primary-bg-color)`}
+      >
+        <main>
+          {children}
+        </main>
+      </body>
+    </html>
+  );
+}
